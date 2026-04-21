f5669d28321817d9f1cd13d1ebf1212e3716226d
gk88vegas <ruanshan4896@gmail.com>
2026-04-21 16:04:43 +0530

    ctrlz

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { Facebook, Youtube, Twitter } from 'lucide-react'
 
 const HOT_LEAGUES = [
   { id: 39,  name: 'Premier League',   href: '/giai-dau/39' },
@@ -56,7 +55,10 @@ export default function Footer() {
                 aria-label="Facebook"
                 className="rounded-full bg-gray-700 p-2 hover:bg-green-700 transition-colors"
               >
-                <Facebook size={16} />
+                {/* Facebook */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
+                </svg>
               </a>
               <a
                 href="https://youtube.com"
@@ -65,7 +67,11 @@ export default function Footer() {
                 aria-label="YouTube"
                 className="rounded-full bg-gray-700 p-2 hover:bg-green-700 transition-colors"
               >
-                <Youtube size={16} />
+                {/* YouTube */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                  <path d="M22.54 6.42a2.78 2.78 0 0 0-1.95-1.96C18.88 4 12 4 12 4s-6.88 0-8.59.46A2.78 2.78 0 0 0 1.46 6.42 29 29 0 0 0 1 12a29 29 0 0 0 .46 5.58 2.78 2.78 0 0 0 1.95 1.96C5.12 20 12 20 12 20s6.88 0 8.59-.46a2.78 2.78 0 0 0 1.95-1.96A29 29 0 0 0 23 12a29 29 0 0 0-.46-5.58z"/>
+                  <polygon fill="#111827" points="9.75 15.02 15.5 12 9.75 8.98 9.75 15.02"/>
+                </svg>
               </a>
               <a
                 href="https://twitter.com"
@@ -74,7 +80,10 @@ export default function Footer() {
                 aria-label="Twitter / X"
                 className="rounded-full bg-gray-700 p-2 hover:bg-green-700 transition-colors"
               >
-                <Twitter size={16} />
+                {/* X (Twitter) */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                </svg>
               </a>
             </div>
           </div>
